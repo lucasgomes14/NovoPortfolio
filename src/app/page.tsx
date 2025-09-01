5104d428b35ab7b3b837e12203e273090df2b987
lucasgomes14 <lucazmatehus14@gmail.com>
2025-09-01 18:58:50 -0300

    chore - header fixo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,58 +9,56 @@ export default function Home() {
     return (
         <div className="bg-[#1B1717] h-full w-screen font-poppins">
             <Background />
-            <div
-                className={"fixed top-10 left-20 w-72 h-72 rounded-full bg-radial from-white/40 to-transparent blur-3xl -z-10"}></div>
             <div className="max-w-7xl mx-auto">
-                <header className="w-full flex items-center justify-center relative pt-7">
+                <header className="fixed top-0 left-0 w-full flex items-center justify-center bg-[#1B1717] z-50 py-8">
                     <div className="absolute left-0">
                         <Image src="/lmgl.png" alt="Logo da página" height={150} width={150}/>
                     </div>
                     <ul className="flex items-center space-x-6 text-xl text-[#9A0101] absolute left-1/2 transform -translate-x-1/2">
                         <li>
                             <a href={"#main"} className="relative cursor-pointer text-[#9A0101] hover:text-[#C90000]
-                  after:absolute after:left-0 after:bottom-0 after:h-0.5 after:w-0
-                  after:bg-[#C90000] after:transition-all after:duration-300
-                  hover:after:w-full">
+                                  after:absolute after:left-0 after:bottom-0 after:h-0.5 after:w-0
+                                  after:bg-[#C90000] after:transition-all after:duration-300
+                                  hover:after:w-full">
                                 Início
                             </a>
                         </li>
                         <li>
                             <a href={"#sobreMim"} className="relative cursor-pointer text-[#9A0101] hover:text-[#C90000]
-                  after:absolute after:left-0 after:bottom-0 after:h-0.5 after:w-0
-                  after:bg-[#C90000] after:transition-all after:duration-300
-                  hover:after:w-full">
+                                  after:absolute after:left-0 after:bottom-0 after:h-0.5 after:w-0
+                                  after:bg-[#C90000] after:transition-all after:duration-300
+                                  hover:after:w-full">
                                 Sobre mim
                             </a>
                         </li>
                         <li>
                             <a className="relative cursor-pointer text-[#9A0101] hover:text-[#C90000]
-                  after:absolute after:left-0 after:bottom-0 after:h-0.5 after:w-0
-                  after:bg-[#C90000] after:transition-all after:duration-300
-                  hover:after:w-full">
+                                  after:absolute after:left-0 after:bottom-0 after:h-0.5 after:w-0
+                                  after:bg-[#C90000] after:transition-all after:duration-300
+                                  hover:after:w-full">
                                 Tecnologias
                             </a>
                         </li>
                         <li>
                             <a className="relative cursor-pointer text-[#9A0101] hover:text-[#C90000]
-                  after:absolute after:left-0 after:bottom-0 after:h-0.5 after:w-0
-                  after:bg-[#C90000] after:transition-all after:duration-300
-                  hover:after:w-full">
+                                  after:absolute after:left-0 after:bottom-0 after:h-0.5 after:w-0
+                                  after:bg-[#C90000] after:transition-all after:duration-300
+                                  hover:after:w-full">
                                 Projetos
                             </a>
                         </li>
                         <li>
                             <a className="relative cursor-pointer text-[#9A0101] hover:text-[#C90000]
-                  after:absolute after:left-0 after:bottom-0 after:h-0.5 after:w-0
-                  after:bg-[#C90000] after:transition-all after:duration-300
-                  hover:after:w-full">
+                                  after:absolute after:left-0 after:bottom-0 after:h-0.5 after:w-0
+                                  after:bg-[#C90000] after:transition-all after:duration-300
+                                  hover:after:w-full">
                                 Contatos
                             </a>
                         </li>
                     </ul>
                 </header>
                 <main className="min-h-screen flex flex-col items-center justify-center">
-                    <section id={"main"} className={"flex flex-col text-center w-1/2"}>
+                <section id={"main"} className={"flex flex-col justify-center text-center w-1/2 h-screen"}>
                         <h1 className={"flex flex-col text-[#909595] mb-4"}>
                             <span className={"text-2xl"}>Olá, eu sou </span>
                             <span className={"font-bold text-3xl"}>Lucas Matheus </span>
@@ -83,7 +81,7 @@ export default function Home() {
                             </Button>
                         </div>
                     </section>
-                    <section id={"sobreMim"} className={""}>
+                    <section id={"sobreMim"} className={"pb-200"}>
                     </section>
                 </main>
             </div>
--- src/components/ui/Background.tsx
+++ src/components/ui/Background.tsx
@@ -35,25 +35,34 @@ export default function Background() {
         };
     };
 
-    const circle1 = ellipseMotion(80, 50, .1);   // raio 50px, velocidade lenta
-    const circle2 = circleMotion(50, 0.1, 90); // raio 80px, com offset
-    const circle3 = imperfectMotion(30, 0.2, 0.01);
+    const circle1 = ellipseMotion(80, 50, .1);
+    const circle2 = circleMotion(50, 0.1, 90);
+    const circle3 = imperfectMotion(10, 10, .001);
     const circle4 = circleMotion(20, 0.4, 270);
+    const circle5 = circleMotion(10, .1);
 
     return (
         <div>
             <div
                 className="fixed top-30 left-50 size-30 rounded-full bg-radial from-[#C90000]/70 to-transparent blur-2xl animate-[pulse_8s_ease_infinite]"
-                style={{ transform: `translate(${circle1.x}px, ${circle1.y}px)` }}></div>
+                style={{ transform: `translate(${circle1.x}px, ${circle1.y}px)` }}>
+            </div>
             <div
                 className="fixed top-90 left-20 size-50 rounded-full bg-radial from-[#9A0101]/30 to-transparent blur-lg animate-[pulse_6s_ease_infinite_reverse]"
-                style={{ transform: `translate(${circle2.x}px, ${circle2.y}px)` }}></div>
+                style={{ transform: `translate(${circle2.x}px, ${circle2.y}px)` }}>
+            </div>
             <div
                 className="fixed top-40 right-80 size-30 rounded-full bg-radial from-[#909595]/50 to-transparent blur-2xl animate-[pulse_4s_ease_infinite]"
-                style={{ transform: `translate(${circle3.x}px, ${circle3.y}px)` }}></div>
+                style={{ transform: `translate(${circle3.x}px, ${circle3.y}px)` }}>
+            </div>
             <div
                 className="fixed top-100 right-30 size-30 rounded-full bg-radial from-[#9A0101]/35 to-transparent blur-2xl animate-[pulse_4s_ease_infinite]"
-                style={{ transform: `translate(${circle4.x}px, ${circle4.y}px)` }}></div>
+                style={{ transform: `translate(${circle4.x}px, ${circle4.y}px)` }}>
+            </div>
+            <div
+                className="fixed top-30 right-20 size-10 rounded-full bg-radial from-[#F2F5FA] to-transparent blur-2xl animate-[pulse_4s_ease_infinite]"
+                style={{ transform: `translate(${circle5.x}px, ${circle5.y}px)` }}>
+            </div>
         </div>
     );
 };
